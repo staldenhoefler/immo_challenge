f3f50e2bf01065db62c82838a1fe3b699439863a
denis.schatzmann@students.fhnw.ch <denis.schatzmann@students.fhnw.ch>
2024-09-23 15:41:22 +0200

    Changed to camelCase

--- test/test_ImportData.py
+++ test/test_ImportData.py
@@ -2,10 +2,6 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    '''
-    Test the createDataBase.py file
-    '''
-
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
