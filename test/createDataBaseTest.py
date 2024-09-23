b7521871f5201c090392d14ea78d9081aa3b2de9
denis.schatzmann@students.fhnw.ch <denis.schatzmann@students.fhnw.ch>
2024-09-23 15:24:39 +0200

    Test for Docstrings in linter

--- test/createDataBaseTest.py
+++ test/createDataBaseTest.py
@@ -2,6 +2,10 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
+    '''
+    Test the createDataBase.py file
+    '''
+
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
